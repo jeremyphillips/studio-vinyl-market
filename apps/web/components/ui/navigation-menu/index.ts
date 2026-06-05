a77f685436d433402c525d78a327bbbc80a37b08
Jeremy Phillips <kidradio@gmail.com>
2026-06-05 13:31:08 -0500

    chore: cleans up unused barrel re-exports

--- apps/web/components/ui/navigation-menu/index.ts
+++ apps/web/components/ui/navigation-menu/index.ts
@@ -2,9 +2,6 @@ export {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
-  NavigationMenuContent,
-  NavigationMenuTrigger,
   NavigationMenuLink,
-  NavigationMenuViewport,
 } from './navigation-menu.client'
 export { navigationMenuTriggerStyle } from './navigation-menu.variants'
